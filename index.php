57d4100a9062c498d10c130a92a13d36fb4453fc
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-10-08 17:44:51 +0200

    static grid layout for album cards

--- index.php
+++ index.php
@@ -16,20 +16,84 @@
 </head>
 
 <body>
+    <header>
+        <h1 class="text-light text-center"> PHP DISCHI</h1>
+    </header>
+    <main class="container mt-5">
+        <div class="row">
+            <div class="col-4">
+                <div class="card mb-5" style="width: 18rem;">
+                    <img class="card-img-top" src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="L.jpg">
+                    <div class="card-body">
+                        <h3 class="card-title text-center text-light">nome disco</h3>
+                        <p class="text-center text-light"> band</p>
+                        <h4 class="text-center text-light">anno</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card mb-5" style="width: 18rem;">
+                    <img class="card-img-top" src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="L.jpg">
+                    <div class="card-body">
+                        <h3 class="card-title text-center text-light">nome disco</h3>
+                        <p class="text-center text-light"> band</p>
+                        <h4 class="text-center text-light">anno</h4>
 
-    <div id="app" class="text-warning">hello
-        {{message}}
-    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card mb-5" style="width: 18rem;">
+                    <img class="card-img-top" src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="L.jpg">
+                    <div class="card-body text-center">
+                        <h3 class="card-title text-center text-light">nome disco</h3>
+                        <p class="text-center text-light"> band</p>
+                        <h4 class="text-center text-light">anno</h4>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                <div class="card mb-5" style="width: 18rem;">
+                    <img class="card-img-top" src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="L.jpg">
+                    <div class="card-body text-center">
+                        <h3 class="card-title text-center text-light">nome disco</h3>
+                        <p class="text-center text-light"> band</p>
+                        <h4 class="text-center text-light">anno</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card mb-5" style="width: 18rem;">
+                    <img class="card-img-top" src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="L.jpg">
+                    <div class="card-body text-center">
+                        <h3 class="card-title text-center text-light">nome disco</h3>
+                        <p class="text-center text-light"> band</p>
+                        <h4 class="text-center text-light">anno</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card mb-5" style="width: 18rem;">
+                    <img class="card-img-top" src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="L.jpg">
+                    <div class="card-body">
+                        <h3 class="card-title text-center text-light">nome disco</h3>
+                        <p class="text-center text-light"> band</p>
+                        <h4 class="text-center text-light">anno</h4>
+
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
 
 
 
 
-
-
-
-
+    </main>
     <!--Axios-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.7.7/axios.min.js" integrity="sha512-DdX/YwF5e41Ok+AI81HI8f5/5UsoxCVT9GKYZRIzpLxb8Twz4ZwPPX+jQMwMhNQ9b5+zDEefc+dcvQoPWGNZ3g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!--Vue import-->
